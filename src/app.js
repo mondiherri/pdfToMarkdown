d36812401f22c328c9cf8e558bf9ade445a7ddff
Edmond <229284662+mondiherri@users.noreply.github.com>
2025-11-17 21:06:48 +0100

    Improve PDF rendering on HiDPI screens

--- src/app.js
+++ src/app.js
@@ -120,12 +120,19 @@ async function goToPage(pageNumber) {
 async function renderPage(pageNumber) {
   const page = await pdfDoc.getPage(pageNumber);
   const viewport = page.getViewport({ scale: 1.25 });
-  canvas.height = viewport.height;
-  canvas.width = viewport.width;
+  const outputScale = window.devicePixelRatio || 1;
+
+  canvas.style.width = `${Math.floor(viewport.width)}px`;
+  canvas.style.height = `${Math.floor(viewport.height)}px`;
+  canvas.width = Math.floor(viewport.width * outputScale);
+  canvas.height = Math.floor(viewport.height * outputScale);
+
+  const transform = outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
 
   const renderContext = {
     canvasContext: ctx,
     viewport,
+    transform,
   };
 
   await page.render(renderContext).promise;
